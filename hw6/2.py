822d700ce33c2a6b3a47d6f2e0883538f2ce59aa
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2025-01-10 09:19:01 +0500

    Add one more graph

--- hw6/2.py
+++ hw6/2.py
@@ -17,9 +17,26 @@ def main():
     plt.clf()
     plt.pie(df['titleType'].value_counts().to_list(), labels=df['titleType'].value_counts().index,
             wedgeprops={'lw': 1, 'ls': '--', 'edgecolor': "k"})
-    plt.title("График распределения по типов произведений")
+    plt.title("График распределения типов произведений")
     plt.savefig("data/title_type_distribution.png")
     plt.clf()
+    df_2020 = df[df["startYear"] == 2020]
+    d = dict()
+    for i in df_2020.index:
+        s = df.loc[i]["genres"]
+        if not type(s) is str:
+            continue
+        s = s.lower()
+        for genre in s.split(","):
+            if genre not in d:
+                d[genre] = 0
+            d[genre] += 1
+    plt.pie(d.values(), labels=d.keys(),
+            wedgeprops={'lw': 1, 'ls': '--', 'edgecolor': "k"})
+    plt.title("График распределения типов произведений в 2020 году")
+    plt.savefig("data/title_type_distribution_2020.png")
+    plt.clf()
+
 
 if __name__ == '__main__':
     main()
